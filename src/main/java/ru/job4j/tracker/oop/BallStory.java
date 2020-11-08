6614519b0c796843bb84fbe574361db8bd1c46c3
Mazay98 <Vova2711vong19980710>
2020-11-08 18:18:15 +0300

    1.6. Взаимодействие объектов [#385233] - fix

--- src/main/java/ru/job4j/tracker/oop/BallStory.java
+++ src/main/java/ru/job4j/tracker/oop/BallStory.java
@@ -1,26 +1,5 @@
 package ru.job4j.tracker.oop;
 
-class Ball {
-}
-
-class Hare {
-    public void tryEat(Ball ball) {
-
-    }
-}
-
-class Wolf {
-    public void tryEat(Fox fox) {
-
-    }
-}
-
-class Fox {
-    public void tryEat(Hare hare) {
-
-    }
-}
-
 public class BallStory {
     public static void main(String[] args) {
         Ball ball = new Ball();
